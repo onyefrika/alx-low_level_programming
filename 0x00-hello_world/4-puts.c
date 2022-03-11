59c0644c69c07713faeea042d901d44e7b9ef346
onyefrika <jennyfaazubuko@gmail.com>
2022-03-11 02:00:50 +0100

    first

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ *  main - Prints String
+ *  Description: "\"Programming is like building a multilingual puzzle"
+ *  Return: 0
+**/
+
+int main(void)
+
+{
+
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+
+}
