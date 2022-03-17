4ea7807a74dae9d5d0d85a10fa63c76a3dcf5418
onyefrika <jennyfaazubuko@gmail.com>
2022-03-17 09:51:33 +0100

    debug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 /**
+<<<<<<< HEAD
  *  main - Prints String
  *  
  *  Return: Always 0 (Success)
+=======
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+>>>>>>> d186a0d03ebd77f4be7654c23e70112e2f255da5
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> d186a0d03ebd77f4be7654c23e70112e2f255da5
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,6 @@
+/**
+ * C program to check whether a character is uppercase 
+ * or lowercase using inbuilt library functions
+ */
+int _isupper(int c);
+
